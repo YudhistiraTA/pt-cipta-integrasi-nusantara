2a1aa72221296ead3fa1971d3146783244ca72a9
Yudhistira Tribuana Authar <yudhistira.tribuana@gmail.com>
2023-07-17 20:36:16 +0700

    Added registration error handling

--- middlewares/errorHandling.js
+++ middlewares/errorHandling.js
@@ -1,7 +1,22 @@
-module.exports = (err, req, res) => {
+module.exports = (err, req, res, next) => {
 	switch (err.name) {
+		case "SequelizeUniqueConstraintError":
+			res.status(400).json({
+				message: "Unique constraint error",
+				err_messages: [err.message]
+			});
+			break;
+		case "SequelizeValidationError":
+			res.status(400).json({
+				message: "Validation error",
+				err_messages: err.errors.map((detail) => detail.message)
+			});
+			break;
 		default:
-            console.log(err);
-			res.status(500).json({ message: "Internal server error" });
+			console.log(err.name);
+			res.status(500).json({
+				message: "Internal server error",
+				err_messages: []
+			});
 	}
 };
