1aa1a11cf1c6c0a58ff892a64d684128bf7d80d5
Yudhistira Tribuana Authar <yudhistira.tribuana@gmail.com>
2023-07-17 21:32:18 +0700

    Changed http request methods for login and register APIs; Added documentation in README.md

--- routers/index.js
+++ routers/index.js
@@ -3,8 +3,8 @@ const authentication = require("../middlewares/authentication");
 const UserController = require("../controllers");
 const router = express.Router();
 
-router.get("/api/register", UserController.register);
-router.get("/api/login", UserController.login);
+router.post("/api/register", UserController.register);
+router.post("/api/login", UserController.login);
 router.get("/api/me", authentication, UserController.detail);
 
 module.exports = router;
